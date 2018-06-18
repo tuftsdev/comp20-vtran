e2af53d37991338b3fcbda05fb47b2685b413c49
Victoria Tran <victoriatran@Sam-Smith5s-MacBook-Air.local>
2018-06-17 19:35:36 -0700

    JavaScript complete

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,15 @@
 // Your JavaScript goes here...
+function parse() {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			var text = JSON.parse(request.responseText);
+	      	document.getElementById("messages").innerHTML = text[0]['content'] + " " + 
+	        text[0]['username'] + "<br />" +  "<br />" + text[1]['content'] + " " + 
+	        text[1]['username'];
+			}	
+	};
+	request.open("GET", "data.json", true);
+	//request.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+	request.send();
+};
